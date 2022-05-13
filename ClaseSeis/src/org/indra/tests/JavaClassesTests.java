dfbf363a0c62afeb0d1bcdf5a4678dcdf04e43e9
Aunión Quirós <iaunion@indra.es>
2022-05-13 11:58:03 +0200

    commit

--- ClaseSeis/src/org/indra/tests/JavaClassesTests.java
+++ ClaseSeis/src/org/indra/tests/JavaClassesTests.java
@@ -2,6 +2,7 @@ package org.indra.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.*;
+import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.Test;
 
@@ -48,5 +49,51 @@ class JavaClassesTests {
 		String elem = pila.pop();
 		assertEquals("Tres",elem);
 	}
+	
+	@Test
+	void testDictionary() {
+		Dictionary<Integer, String> diccionario = new Hashtable<Integer, String>();
+		diccionario.put(1, "Uno");
+		diccionario.put(2, "Dos");
+		diccionario.put(3, "Tres");
+		assertEquals("Uno",diccionario.get(1));
+		assertEquals("Dos",diccionario.get(2));
+		assertEquals("Tres",diccionario.get(3));
+	}
+	
+	@Test
+	void testSet() {
+		Set<String> conjunto = new TreeSet<String>();
+		conjunto.add("a");
+		conjunto.add("a");
+		conjunto.add("a");
+		conjunto.add("b");
+		conjunto.add("b");
+		conjunto.add("b");
+		assertEquals(2,conjunto.size());
+	}
+	
+	@Test
+	void testDate() {
+		Date fecha = new Date (2022,1,1);
+		assertEquals(2022,fecha.getYear());
+	}
+	
+	@Test
+	void testCalendar() {
+		Calendar fecha = new GregorianCalendar(2020,1,1);
+		assertEquals(2020,fecha.get(Calendar.YEAR));
+		
+	}
+	
+	@Test
+	void testUUID() {
+		assertNotEquals(UUID.randomUUID(), UUID.randomUUID());
+	}
 
+	@Test
+	void testRegex() {
+		assertEquals(true, Pattern.matches("[0-9]*", "1233343"));
+		assertNotEquals(true, Pattern.matches("[0-9]*", "1233343A"));
+	}
 }
---
+++ ClaseSeis/src/org/indra/tests/JavaFileTests.java
@@ -0,0 +1,36 @@
+package org.indra.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.*;
+import java.util.*;
+
+import org.junit.jupiter.api.Test;
+
+class JavaFileTests {
+
+	@Test
+	void testNewFile() throws IOException {
+		File archivo = new File("text.txt");
+		archivo.createNewFile();
+		assertEquals(true, archivo.exists());
+	}
+	
+	@Test
+	void testWriteFile() throws IOException {
+		FileWriter writer = new FileWriter("text.txt");
+		writer.write("Este es un texto");
+		writer.close();
+		
+		File archivo = new File("text.txt");
+		Scanner reader = new Scanner(archivo);
+		String linea = "";
+		if (reader.hasNextLine()) {
+			linea = reader.nextLine();
+		}
+		
+		assertEquals("Este es un texto", linea);
+		reader.close();
+	}
+
+}
